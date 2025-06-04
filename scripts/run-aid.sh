908e15a103120772f8a9ed7d30ce47a1113d6863
Davi Reis <daviavr@gmail.com>
2025-06-04 00:21:16 -0300

    readding missing scripts and changing maxdepth in files plugin in albert

---
+++ scripts/run-aid.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+backend_path=~/nonblobs/work/clinicmanager/backend
+backend="$backend_path/mvnw spring-boot:run -f $backend_path/pom.xml"
+
+gbackend_path=~/nonblobs/work/general
+gbackend="$gbackend_path/mvnw spring-boot:run -f $gbackend_path/pom.xml"
+
+tmux new-session -ds run-backend $backend 
+tmux new-session -ds run-gbackend $gbackend 
